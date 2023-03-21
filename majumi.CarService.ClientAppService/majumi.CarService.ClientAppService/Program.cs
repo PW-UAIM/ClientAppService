cddde7932ef46137d6613da0ecc5e58ff65a5640
K3rne1 <mtjurczak@gmail.com>
2023-03-22 00:51:15 +0100

    Base service with trash

---
+++ majumi.CarService.ClientAppService/majumi.CarService.ClientAppService/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.OpenApi.Models;
+
+namespace majumi.CarService.ClientAppService {
+    public class Program {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ClientAppService", Description = "ClientAppService", Version = "v1" });
+            });
+
+            var app = builder.Build();
+           
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseStatusCodePages();
+            app.UseRouting();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ClientAppService");
+            });
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+
+            app.Run();
+        }
+    }
+}
