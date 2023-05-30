837b9411be7c8b916f7208c87ffc3251b959ca40
unknown <01168941@pw.edu.pl>
2023-05-30 21:15:24 +0200

    Dockerize

---
+++ Docker/RunImage.bat
@@ -0,0 +1,21 @@
+docker login -u majumi -p uaimrzadzi
+
+docker ps -a
+
+docker stop clientappservice
+
+docker ps 
+
+docker images
+
+::docker pull majumi/clientappservice:appservice
+
+docker run --name clientappservice -p 5010:80 -it majumi/clientappservice:appservice
+
+pause
+
+docker stop clientappservice
+
+docker rm clientappservice
+
+pause
