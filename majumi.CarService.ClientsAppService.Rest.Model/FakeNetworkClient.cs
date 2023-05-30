9222c10fa7fd9706c9ba3cc3c7977fe0de3e13c2
unknown <01168941@pw.edu.pl>
2023-05-30 21:15:43 +0200

    Dockerize

---
+++ majumi.CarService.ClientsAppService.Rest.Model/FakeNetworkClient.cs
@@ -0,0 +1,18 @@
+﻿namespace ZsutPw.Patterns.Application.Model;
+
+using majumi.CarService.ClientsAppService.Rest.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class FakeNetworkClient
+{
+    private static readonly CarData[] cars = new CarData[] { new CarData() { CarID = 1, Make = "Ford", Model = "Focus", Year = 2002, Mileage = 10000, EngineSize = "1.1", VIN = "123123", LicensePlate = "WPL12345", ClientID = 1 } };
+
+    public CarData[] GetCarsByClientID(int clientID)
+    {
+        return cars.Where(car => car.ClientID == clientID).ToArray();
+    }
+}
---
+++ majumi.CarService.ClientsAppService.Rest.Model/Model/Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZsutPw.Patterns.Application.Model;
+
+namespace majumi.CarService.ClientsAppService.Rest.Model.Model;
+
+public class Model
+{
+    public int ClientID { get; set; }
+
+    public CarData[] LoadCarList()
+    {
+        FakeNetworkClient client = new();
+        return client.GetCarsByClientID(ClientID);
+    }
+}
